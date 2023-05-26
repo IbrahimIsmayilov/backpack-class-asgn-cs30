848549ca357583d74a62acb3fd642da44d53111b
Ibrahim Ismayilov <i.ismayilov@EDU.epsb.ca>
2023-05-26 11:22:58 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+# Backpack Class Assignment by Ibrahim Ismayilov
+
+# Chaacter Class Assignment by Ibrahim Ismayilov
+
+# TASK 1
+class Backpack:
+    # Constructor: Properties (State)
+    def __init__(self, color, size):
+        self.color = color
+        self.size = size
+        self.items = []
+        self.open = False
+    # Methods (Behaviour)
+
+    # Print Out Character’s Primary or Secondary Catch Phrase Based on the PhraseNum Argument.
+
+        
+
+# TASK 2
+kung_fu_panda = Character("Kung Fu Panda", "Skadoosh", "You have been blinded by pure awesomeness!")
+
+spiderman = Character("Spiderman", "My Spider-Sense is tingling", "Your friendly neighbourhood spiderman.")
+
+# TASK 3
+kung_fu_panda.speak(1)
+kung_fu_panda.setLevel(2)
+spiderman.speak(2)
+
